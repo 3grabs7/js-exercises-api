f1cbdf605187627e715f297b93ba5c0c34d015c4
Robin Grabs <robinmgrabs@gmail.com>
2022-01-16 13:13:28 +0100

    Add project files.

---
+++ JsExerciseAPI/Controllers/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JsExerciseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private string[] _imageUrls = new string[] {
+            "https://img-9gag-fun.9cache.com/photo/a3Q5VW5_460s.jpg",
+            "https://media.moddb.com/images/members/5/4550/4549205/duck.jpg",
+            "https://www.kibrispdr.org/dwn/5/random-pic.jpg"
+        };
+
+        private Random _rnd = new();
+
+        [HttpGet("{id}")]
+        public ActionResult GetImage(int id)
+        {
+            if (!Enumerable.Range(0, _imageUrls.Length).Contains(id)) return NotFound();
+
+            Thread.Sleep(_rnd.Next(2, 10) * 1000);
+
+            return Ok(new { url = _imageUrls[(int)id] });
+        }
+
+
+    }
+}
